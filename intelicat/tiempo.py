90d8a74001d8c6cbd35e94e3f5c65d3fe3a3a074
Alejandro Rondón <70446436+Ale-06@users.noreply.github.com>
2023-01-24 14:15:21 -0400

    Update tiempo.py

--- intelicat/tiempo.py
+++ intelicat/tiempo.py
@@ -1,14 +1,18 @@
 import time
-      # Ralentizado entre mensajes
-def sleep1():
-      time.sleep(1)
-def sleep2():
-      time.sleep(2)
-def sleep3():
-      time.sleep(3)
-def sleep4():
-      time.sleep(4)
-
       # Espacio entre diálogos
 def espacio():
       print("\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n")
+      # Ralentizado entre mensajes
+def sleep1():
+      time.sleep(1)
+      espacio()
+def sleep2():
+      time.sleep(2)
+      espacio()
+def sleep3():
+      time.sleep(3)
+      espacio()
+def sleep4():
+      time.sleep(4)
+      espacio()
+     
